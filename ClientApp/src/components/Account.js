eb5468661f706bd9cbdd9241324f7761fabbe2d0
Anna Dodson <anna@annadodson.co.uk>
2019-09-26 22:50:38 +0100

    Fix for Account page broken links
    - Fixes #19

--- ClientApp/src/components/Account.js
+++ ClientApp/src/components/Account.js
@@ -12,11 +12,11 @@ export class Account extends Component {
         <h3>Oopes this page doesn't really exist yet.</h3>
         <p>This web app is still being built.</p>
         <br />
-        <p>You can take a look at the progress on <Link to="https://github.com/AnnaDodson/step-challenge">GitHub</Link>. All contributions welcome.</p>
+        <p>You can take a look at the progress on <a target="_blank" href="https://github.com/AnnaDodson/step-challenge">GitHub</a>. All contributions welcome.</p>
         <h3>Found a bug?</h3>
-        <p>Raise an issue <Link to="https://github.com/AnnaDodson/step-challenge/issues">Here</Link>.</p>
+        <p>Raise an issue <a target="_blank" href="https://github.com/AnnaDodson/step-challenge/issues">Here</a>.</p>
         <h3>Have a request?</h3>
-        <p>Make an issue <Link to="https://github.com/AnnaDodson/step-challenge/issues">Here</Link>.</p>
+        <p>Make an issue <a target="_blank" href="https://github.com/AnnaDodson/step-challenge/issues">Here</a>.</p>
       </div>
     );
   }
